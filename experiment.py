f70aac840d41f0e3cb3837a45f43ffc4aa1a92d3
Fede <fede.garza.ramirez@gmail.com>
2020-07-10 17:23:37 -0500

    adding module to test and fixing typo

---
+++ experiment.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+
+import pandas as pd
+
+from ESRNN.m4_data import prepare_m4_data, seas_dict
+from ESRNN.utils_evaluation import evaluate_prediction_owa
+from fforma.metrics import WeightedPinballLoss
+from fforma.meta_learner import MetaLearnerNN
+from fforma.meta_results_r_data import prepare_fforma_data
+#Freqs used by hyndman
+freqs = {'Hourly': 24, 'Daily': 1,
+         'Monthly': 12, 'Quarterly': 4,
+         'Weekly':1, 'Yearly': 1}
+
+
+def evaluate_fforma(dataset_name, fforma_df, directory, num_obs):
+    _, y_train_df, X_test_df, y_test_df = prepare_m4_data(dataset_name=dataset_name,
+                                                          directory=directory,
+                                                          num_obs=num_obs)
+
+    y_test_fforma = fforma_df[fforma_df['unique_id'].isin(y_test_df['unique_id'].unique())]
+    y_test_fforma = y_test_fforma.rename(columns={'fforma_prediction': 'y_hat'})
+    y_test_fforma = y_test_fforma.filter(items=['unique_id', 'ds', 'y_hat'])
+
+    seasonality = freqs[dataset_name]
+    owa, mase, smape = evaluate_prediction_owa(y_test_fforma, y_train_df,
+                                               X_test_df, y_test_df,
+                                               seasonality)
+
+    return dataset_name, owa, mase, smape
+
+def main(args):
+    dataset_name = args.dataset
+    directory = args.directory
+    hyndman_data = directory + '/hyndman_data'
+    # ## Meta results from R (hyndman, et al)
+    feats_train, X_models_train, \
+        y_models_train, feats_test, \
+        X_models_test = prepare_fforma_data(hyndman_data, dataset_name=dataset_name) #dataset_name=None for all data
+
+    h = seas_dict[dataset_name]['output_size']
+
+    df = X_models_train.groupby(['unique_id']).size()
+    uids = df[df == h].index
+
+    X_models_train = X_models_train[X_models_train['unique_id'].isin(uids)]
+    y_models_train = y_models_train[y_models_train['unique_id'].isin(uids)]
+    feats_train = feats_train[feats_train['unique_id'].isin(uids)]
+
+    feats_train = feats_train.set_index(['unique_id'])
+    X_models_train = X_models_train.set_index(['unique_id', 'ds'])
+    y_models_train = y_models_train.set_index(['unique_id', 'ds'])
+
+    nn_params = {'layers': [100, 50, 25, 10],
+                 'dropout': 0.1,
+                 'epochs': 2,
+                 'batch_size': 2,
+                 'learning_rate': 5e-4,
+                 'gradient_eps': 1e-8,
+                 'weight_decay': 0,
+                 'freq_of_test': 5,
+                 'loss_function': WeightedPinballLoss(0.4)}
+
+    model = MetaLearnerNN(nn_params, X_models_train, y_models_train, h)
+
+    model.fit(feats_train, verbose_eval=True)
+
+    feats_test_id = feats_test.set_index('unique_id')
+    X_models_test_id = X_models_test.set_index(['unique_id', 'ds'])
+
+    fforma_predictions = model.predict(feats_test_id)
+    fforma_predictions = pd.DataFrame(fforma_predictions,
+                                      index=feats_test_id.index,
+                                      columns=X_models_test_id.columns)
+
+    preds = (fforma_predictions * X_models_test_id).sum(1)
+    preds = preds.rename('y_hat').to_frame().reset_index()
+
+    # # Evaluation
+
+    evaluation = evaluate_fforma(dataset_name, preds, directory, 100_000)
+
+    print(evaluation)
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Test QFFORMA on M4 data')
+    parser.add_argument("--dataset", required=True, type=str,
+                      choices=['Yearly', 'Quarterly', 'Monthly', 'Weekly', 'Hourly', 'Daily'],
+                      help="set of M4 time series to be tested")
+    parser.add_argument("--directory", required=True, type=str,
+                      help="directory where M4 data will be downloaded")
+
+    args = parser.parse_args()
+
+    main(args)
--- fforma/R/prepare_data_m4.R
+++ fforma/R/prepare_data_m4.R
@@ -25,23 +25,23 @@ clean_series <- function(list_ts, kind = "errors"){
     df <- as_tibble(df)
     df <- mutate(df, ds = 1:nrow(df))
     df <- select(df, ds, everything())
-    
+
   } else if (kind=="freq") {
     ts <- list_ts[["x"]]
     df <- as_tibble(list(freq = frequency(ts), class_ts = class(ts)))
-    
+
   } else if (kind=="xx") {
     ts <- as.vector(list_ts[[kind]])
     df <- enframe(ts, 'ds')
     df <- rename(df, y = value)
-    
+
   } else {
     df <- as_tibble(as.list(list_ts[[kind]]))
   }
-  
+
   df <- mutate(df, unique_id = list_ts$st)
   df <- select(df, unique_id, everything())
-  
+
   return(df)
 }
 
@@ -64,31 +64,29 @@ if(!dir.exists(dir_processed)){
 print("Saving train data...")
 for(kind in c("features", "ff", "xx")){
   print(kind)
-  
+
   file_kind <- str_glue("{dir_}/processed_data/train-{kind}.csv")
-    
+
   if(!file.exists(file_kind)){
-    meta_M4 %>% 
-      future_map_dfr(clean_series, kind = kind, .progress = TRUE) %>% 
+    meta_M4 %>%
+      future_map_dfr(clean_series, kind = kind, .progress = TRUE) %>%
       fwrite(file_kind)
   }
 }
 
-rm(meta_m4)
+rm(meta_M4)
 
 # Saving prediction phase -------------------------------------------------
 
 print("Saving test data...")
 for(kind in c("features", "ff")){
   print(kind)
-    
+
   file_kind <- str_glue("{dir_}/processed_data/test-{kind}.csv")
-  
+
   if(!file.exists(file_kind)){
-    submission_M4 %>% 
-      future_map_dfr(clean_series, kind = kind, .progress = TRUE) %>% 
+    submission_M4 %>%
+      future_map_dfr(clean_series, kind = kind, .progress = TRUE) %>%
       fwrite(file_kind)
   }
 }
-
-  
