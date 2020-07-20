37d7bf5062d260dfb81c9ab5719555d57e511702
Fede <fede.garza.ramirez@gmail.com>
2020-07-20 11:22:53 -0500

    uploading full m4 data

--- fforma/m4_data.py
+++ fforma/m4_data.py
@@ -8,6 +8,9 @@ import subprocess
 import numpy as np
 import pandas as pd
 
+from fforma.base_models import Naive2
+
+
 seas_dict = {'Hourly': {'seasonality': 24, 'input_size': 24,
                        'output_size': 48, 'freq': 'H'},
              'Daily': {'seasonality': 7, 'input_size': 7,
@@ -25,7 +28,7 @@ SOURCE_URL = 'https://raw.githubusercontent.com/Mcompetitions/M4-methods/master/
 
 
 def maybe_download(filename, directory):
-    """ Download the data from M4's website, unless it's already here.
+    """Download the data from M4's website, unless it's already here.
 
     Parameters
     ----------
@@ -48,14 +51,13 @@ def maybe_download(filename, directory):
     filepath = os.path.join(data_directory, filename)
     if not os.path.exists(filepath):
         filepath, _ = urllib.request.urlretrieve(SOURCE_URL + filename, filepath)
-
         size = os.path.getsize(filepath)
         print('Successfully downloaded', filename, size, 'bytes.')
 
     return filepath
 
 def m4_parser(dataset_name, directory, num_obs=1000000):
-    """ Transform M4 data into a panel.
+    """Transform M4 data into a panel.
 
     Parameters
     ----------
@@ -110,9 +112,9 @@ def m4_parser(dataset_name, directory, num_obs=1000000):
     len_series = df.groupby('unique_id').agg({'ds': 'max'}).reset_index()
     dates = []
     for i in range(len(len_series)):
-      len_serie = len_series.iloc[i,1]
-      ranges = pd.date_range(start='1970/01/01', periods=len_serie, freq=freq)
-      dates += list(ranges)
+        len_serie = len_series.iloc[i,1]
+        ranges = pd.date_range(start='1970/01/01', periods=len_serie, freq=freq)
+        dates += list(ranges)
     df.loc[:,'ds'] = dates
 
     df = df.merge(m4_info, left_on=['unique_id'], right_on=['M4id'])
@@ -131,9 +133,67 @@ def m4_parser(dataset_name, directory, num_obs=1000000):
 
     return X_train_df, y_train_df, X_test_df, y_test_df
 
+def naive2_predictions(dataset_name, directory, num_obs, y_train_df = None, y_test_df = None):
+    """Computes Naive2 predictions.
+
+    Parameters
+    ----------
+    dataset_name: str
+        Frequency of the data. Example: 'Yearly'.
+    directory: str
+        Custom directory where data will be saved.
+    num_obs: int
+        Number of time series to return.
+    y_train_df: DataFrame
+        Y train set returned by m4_parser
+    y_test_df: DataFrame
+        Y test set returned by m4_parser
+    """
+    # Read train and test data
+    if (y_train_df is None) or (y_test_df is None):
+        _, y_train_df, _, y_test_df = m4_parser(dataset_name, directory, num_obs)
+
+    seasonality = seas_dict[dataset_name]['seasonality']
+    input_size = seas_dict[dataset_name]['input_size']
+    output_size = seas_dict[dataset_name]['output_size']
+    freq = seas_dict[dataset_name]['freq']
+
+    print('Preparing {} dataset'.format(dataset_name))
+    print('Preparing Naive2 {} dataset predictions'.format(dataset_name))
+
+    # Naive2
+    y_naive2_df = pd.DataFrame(columns=['unique_id', 'ds', 'y_hat'])
+
+    # Sort X by unique_id for faster loop
+    y_train_df = y_train_df.sort_values(by=['unique_id', 'ds'])
+    # List of uniques ids
+    unique_ids = y_train_df['unique_id'].unique()
+    X_test = pd.DataFrame(range(output_size))
+    # Panel of fitted models
+    for unique_id in unique_ids:
+        # Fast filter X and y by id.
+        top_row = np.asscalar(y_train_df['unique_id'].searchsorted(unique_id, 'left'))
+        bottom_row = np.asscalar(y_train_df['unique_id'].searchsorted(unique_id, 'right'))
+        y_id = y_train_df[top_row:bottom_row]
+
+        y_naive2 = pd.DataFrame(columns=['unique_id', 'ds', 'y_hat'])
+        y_naive2['ds'] = pd.date_range(start=y_id.ds.max(),
+                                   periods=output_size+1, freq=freq)[1:]
+        y_naive2['unique_id'] = unique_id
+        y_naive2['y_hat'] = Naive2(seasonality).fit(None, y_id.y.to_numpy()).predict(X_test)
+        y_naive2_df = y_naive2_df.append(y_naive2)
+
+    y_naive2_df = y_test_df.merge(y_naive2_df, on=['unique_id', 'ds'], how='left')
+    y_naive2_df.rename(columns={'y_hat': 'y_hat_naive2'}, inplace=True)
+
+    results_dir = directory + '/results'
+    naive2_file = results_dir + '/{}-naive2predictions_{}.csv'.format(dataset_name, num_obs)
+    y_naive2_df.to_csv(naive2_file, encoding='utf-8', index=None)
+
+    return y_naive2_df
+
 def prepare_m4_data(dataset_name, directory, num_obs):
-    """Pipeline that obtains M4 times series, tranforms it and
-    gets naive2 predictions.
+    """Pipeline that obtains M4 times series, tranforms it and gets naive2 predictions.
 
     Parameters
     ----------
@@ -163,4 +223,33 @@ def prepare_m4_data(dataset_name, directory, num_obs):
 
     X_train_df, y_train_df, X_test_df, y_test_df = m4_parser(dataset_name, directory, num_obs)
 
-    return X_train_df, y_train_df, X_test_df, y_test_df
+    results_dir = directory + '/results'
+    if not os.path.exists(results_dir):
+        os.mkdir(results_dir)
+
+    naive2_file = results_dir + '/{}-naive2predictions_{}.csv'
+    naive2_file = naive2_file.format(dataset_name, num_obs)
+
+    if not os.path.exists(naive2_file):
+        y_naive2_df = naive2_predictions(dataset_name, directory, num_obs, y_train_df, y_test_df)
+    else:
+        y_naive2_df = pd.read_csv(naive2_file)
+        y_naive2_df['ds'] = pd.to_datetime(y_naive2_df['ds'])
+
+    return X_train_df, y_train_df, X_test_df, y_naive2_df
+
+def prepare_full_m4_data(directory):
+
+    data = []
+    for dataset_name in seas_dict.keys():
+        dataset_data = prepare_m4_data(dataset_name, directory, 100_000)
+        data.append(dataset_data)
+
+    X_train_df, y_train_df, X_test_df, y_naive2_df = zip(*data)
+
+    X_train_df = pd.concat(X_train_df).reset_index(drop=True)
+    y_train_df = pd.concat(y_train_df).reset_index(drop=True)
+    X_test_df = pd.concat(X_test_df).reset_index(drop=True)
+    y_naive2_df = pd.concat(y_naive2_df).reset_index(drop=True)
+
+    return X_train_df, y_train_df, X_test_df, y_naive2_df
