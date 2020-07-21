c6906de19fbdfa52468be654e46120dc7025189b
Fede <fede.garza.ramirez@gmail.com>
2020-07-21 09:40:20 -0500

    adding m3 data

--- fforma/meta_results_r_data.py
+++ fforma/meta_results_r_data.py
@@ -10,8 +10,10 @@ import pandas as pd
 
 from tqdm import tqdm
 from fforma.m4_data import prepare_m4_data, prepare_full_m4_data
+from fforma.utils import wide_to_long
 
-URL = 'https://github.com/pmontman/M4metaresults/releases/download/v0.0.0.9000/M4metaresults_0.0.0.9000.tar.gz'
+URL_M4 = 'https://github.com/pmontman/M4metaresults/releases/download/v0.0.0.9000/M4metaresults_0.0.0.9000.tar.gz'
+URL_M3 = 'https://github.com/FedericoGarza/meta-data/releases/download/v0.0.0.9000/m3-meta-data.pickle'
 
 def execute(cmd):
     popen = subprocess.Popen(cmd, stdout=subprocess.PIPE, universal_newlines=True)
@@ -22,7 +24,11 @@ def execute(cmd):
     if return_code:
         raise subprocess.CalledProcessError(return_code, cmd)
 
-def maybe_download_decompress(directory):
+################################################################################
+#################### M4 processes
+###############################################################################
+
+def maybe_download_decompress_m4(directory):
     """Download the data from M4's website, unless it's already here.
 
     Parameters
@@ -39,12 +45,12 @@ def maybe_download_decompress(directory):
     if not os.path.exists(compressed_data_directory):
         os.mkdir(compressed_data_directory)
 
-    filename = URL.split('/')[-1]
+    filename = URL_M4.split('/')[-1]
     filepath = os.path.join(compressed_data_directory, filename)
 
     if not os.path.exists(filepath):
         # Streaming, so we can iterate over the response.
-        r = requests.get(URL, stream=True)
+        r = requests.get(URL_M4, stream=True)
         # Total size in bytes.
         total_size = int(r.headers.get('content-length', 0))
         block_size = 1024 #1 Kibibyte
@@ -63,7 +69,7 @@ def maybe_download_decompress(directory):
         size = os.path.getsize(filepath)
         print('Successfully downloaded', filename, size, 'bytes.')
 
-    if not data_already_present(directory, kind='decompressed'):
+    if not data_already_present_m4(directory, kind='decompressed'):
         decompressed_data_directory = compressed_data_directory + '/decompressed_data'
 
         tar = tarfile.open(filepath, 'r:gz')
@@ -72,7 +78,7 @@ def maybe_download_decompress(directory):
 
         print('Successfully decompressed ', decompressed_data_directory)
 
-def data_already_present(directory, kind):
+def data_already_present_m4(directory, kind):
     """
     kind: str
         Can be 'r' o 'decompressed'.
@@ -96,13 +102,13 @@ def data_already_present(directory, kind):
 
     return present
 
-def prepare_fforma_data(directory, dataset_name=None):
+def prepare_fforma_data_m4(directory, dataset_name=None):
 
     #Check downloaded data
-    maybe_download_decompress(directory)
+    maybe_download_decompress_m4(directory)
 
     #Prepare data from R
-    if not data_already_present(directory, kind='r'):
+    if not data_already_present_m4(directory, kind='r'):
         cmd = f'Rscript ./fforma/R/prepare_data_m4.R "{directory}"'
         res_r = os.system(cmd)
 
@@ -133,3 +139,100 @@ def prepare_fforma_data(directory, dataset_name=None):
 
 
     return X_train_df, preds_train_df, y_train_df, X_test_df, preds_test_df, y_test_df
+
+################################################################################
+################# M3 processes
+################################################################################
+
+def maybe_download_m3(directory):
+    """Download M3's meta-data, unless it's already here.
+
+    Parameters
+    ----------
+    directory: str
+        Custom directory where data will be downloaded.
+    """
+    root_fforma_data = directory + '/m3_meta_data'
+    if not os.path.exists(root_fforma_data):
+        os.mkdir(root_fforma_data)
+
+    raw_data_directory = root_fforma_data + '/raw'
+
+    if not os.path.exists(raw_data_directory):
+        os.mkdir(raw_data_directory)
+
+    filename = URL_M3.split('/')[-1]
+    filepath = os.path.join(raw_data_directory, filename)
+
+    if not os.path.exists(filepath):
+        # Streaming, so we can iterate over the response.
+        r = requests.get(URL_M3, stream=True)
+        # Total size in bytes.
+        total_size = int(r.headers.get('content-length', 0))
+
+        block_size = 1024 #1 Kibibyte
+        t = tqdm(total=total_size, unit='iB', unit_scale=True)
+
+        with open(filepath, 'wb') as f:
+            for data in r.iter_content(block_size):
+                t.update(len(data))
+                f.write(data)
+
+        t.close()
+
+        if total_size != 0 and t.n != total_size:
+            print("ERROR, something went wrong")
+
+        size = os.path.getsize(filepath)
+        print('Successfully downloaded', filename, size, 'bytes.')
+
+    return filepath
+
+def prepare_fforma_data_m3(directory, dataset_name=None):
+
+    #Check downloaded data
+    filepath = maybe_download_m3(directory)
+
+
+    X_train_df, preds_train_df, X_test_df, preds_test_df = pd.read_pickle(filepath)
+
+    preds_train_df = preds_train_df.drop('horizon', 1).rename(columns={'y_val': 'y'})
+    preds_test_df = preds_test_df.drop('horizon', 1).rename(columns={'y_test': 'y'})
+
+    cols_wide_to_long = ['ds', 'auto_arima_forec', 'ets_forec',
+                         'naive_forec', 'nnetar_forec', 'rw_drift_forec',
+                         'snaive_forec', 'stlm_ar_forec', 'tbats_forec',
+                         'theta_forec', 'y', 'y_hat_naive2']
+
+    preds_train_df = wide_to_long(preds_train_df, cols_wide_to_long)
+    preds_test_df = wide_to_long(preds_test_df, cols_wide_to_long)
+
+    preds_train_df, y_train_df = preds_train_df.drop(['y', 'y_hat_naive2'], 1), preds_train_df[['unique_id', 'ds', 'y', 'y_hat_naive2']]
+    preds_test_df, y_test_df = preds_test_df.drop(['y', 'y_hat_naive2'], 1), preds_test_df[['unique_id', 'ds', 'y', 'y_hat_naive2']]
+
+    if dataset_name is not None:
+        kind = dataset_name[0]
+
+        X_train_df = X_train_df[X_train_df['unique_id'].str.startswith(kind)]
+        preds_train_df = preds_train_df[preds_train_df['unique_id'].str.startswith(kind)]
+        y_train_df = y_train_df[y_train_df['unique_id'].str.startswith(kind)]
+
+        X_test_df = X_test_df[X_test_df['unique_id'].str.startswith(kind)]
+        preds_test_df = preds_test_df[preds_test_df['unique_id'].str.startswith(kind)]
+        y_test_df = y_test_df[y_test_df['unique_id'].str.startswith(kind)]
+
+
+    return X_train_df, preds_train_df, y_train_df, X_test_df, preds_test_df, y_test_df
+
+
+################################################################################
+################# Main function
+################################################################################
+
+def prepare_fforma_data(directory, dataset_name=None, kind='M4'):
+    """
+    """
+    if kind == 'M4':
+        return prepare_fforma_data_m4(directory, dataset_name)
+    elif kind == 'M3':
+        return prepare_fforma_data_m3(directory, dataset_name)
--- fforma/utils.py
+++ fforma/utils.py
@@ -304,3 +304,34 @@ def evaluate_forecasts(dataset_name, panel_df, directory, num_obs):
     evaluation = pd.DataFrame(evaluation, index=[dataset_name])
 
     return evaluation
+
+def wide_to_long(df, lst_cols, fill_value='', preserve_index=False):
+    # make sure `lst_cols` is list-alike
+    if (lst_cols is not None
+        and len(lst_cols) > 0
+        and not isinstance(lst_cols, (list, tuple, np.ndarray, pd.Series))):
+        lst_cols = [lst_cols]
+    # all columns except `lst_cols`
+    idx_cols = df.columns.difference(lst_cols)
+    # calculate lengths of lists
+    lens = df[lst_cols[0]].str.len()
+    # preserve original index values
+    idx = np.repeat(df.index.values, lens)
+    # create "exploded" DF
+    res = (pd.DataFrame({
+                col:np.repeat(df[col].values, lens)
+                for col in idx_cols},
+                index=idx)
+             .assign(**{col:np.concatenate(df.loc[lens>0, col].values)
+                            for col in lst_cols}))
+    # append those rows that have empty lists
+    if (lens == 0).any():
+        # at least one list in cells is empty
+        res = (res.append(df.loc[lens==0, idx_cols], sort=False)
+                  .fillna(fill_value))
+    # revert the original index order
+    res = res.sort_index()
+    # reset index if requested
+    if not preserve_index:
+        res = res.reset_index(drop=True)
+    return res
