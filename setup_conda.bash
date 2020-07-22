1447e61e2582e97886b54e93f92404048ce5f56b
cristianchallu <cristiani.challu@gmail.com>
2020-07-22 10:53:20 -0400

    setup conda

---
+++ setup_conda.bash
@@ -0,0 +1,50 @@
+# ts_transfer packages
+conda create --name fforma_fed python=3.6
+source ~/anaconda3/etc/profile.d/conda.sh
+conda activate fforma_fed
+
+# basic
+conda install nomkl
+conda install -c anaconda numpy==1.16.1
+conda install -c anaconda pandas==0.25.2
+conda install -c anaconda scikit-learn==0.23.1
+conda install -c anaconda scipy==1.4.1
+
+#Forecast
+conda install -c conda-forge statsmodels==0.11.1
+conda install -c r rpy2==3.3.3
+conda install -c conda-forge lightgbm==2.3.1
+conda install -c conda-forge supersmoother
+conda install -c bashtage arch
+pip install stldecompose
+
+# Pytorch
+conda install pytorch=1.3.1 -c pytorch
+
+# ESRNN
+pip install ESRNN==0.1.2
+
+#Other
+conda install -c anaconda patsy==0.5.1
+pip install --user rstl
+pip install threadpoolctl # este porque?
+conda install dask
+#conda install -c conda-forge pathos
+
+# visualization
+conda install -c conda-forge matplotlib==3.2.1
+conda install -c conda-forge tqdm==4.46.0
+
+conda install -c conda-forge jupyterlab
+ipython kernel install --user --name=fforma_fed
+conda install -c anaconda pylint
+conda install -c anaconda pyyaml
+conda deactivate
+
+# Para que se usa C?
+# conda install -c anaconda kiwisolver==1.2.0
+#Cython==0.29.19
+#ESRNN==0.1.2
+#llvmlite==0.32.1
+#numba==0.49.1
+#property-cached==1.6.4
