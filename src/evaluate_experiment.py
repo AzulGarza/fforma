00a8c46a89a3e0dfaf8634cc41776eb1310a79a0
Fede <fede.garza.ramirez@gmail.com>
2020-07-23 20:55:34 -0500

    improving evalaution

--- src/evaluate_experiment.py
+++ src/evaluate_experiment.py
@@ -13,7 +13,16 @@ import glob
 from src.experiment import DICT_FREQS, grid_qfforma, generate_grids
 from src.meta_evaluation import evaluate_fforma_experiment
 
-def evaluate(dataset, generate, results_dir):
+def uploat_evaluation_to_s3(model_id, data, dataset):
+
+    #mc_dict = mc_row.to_dict()
+    #data = {**mc_dict, **evaluation_dict}
+    #data = pd.DataFrame(data, index=[0])
+
+    pickle_file = f's3://research-storage-orax/{dataset}/evaluation/qfforma-evaluation-{model_id}.p'
+    pd.to_pickle(data, pickle_file)
+
+def evaluate(dataset, start_id, end_id, generate, results_dir):
 
     # Read/Generate hyperparameter grid
     if generate:
@@ -22,29 +31,26 @@ def evaluate(dataset, generate, results_dir):
     grid_file_name = results_dir + 'model_grid_qfforma.csv'
     model_specs_df = pd.read_csv(grid_file_name)
 
-    errors = []
     size = len(model_specs_df)
-    for i, (model_id, df) in enumerate(model_specs_df.groupby('model_id')):
+    for idx, i in enumerate(range(start_id, end_id)):
+        mc = model_specs_df.loc[[i], :]
+        model_id = mc['model_id'].item()
 
         s3_file = f's3://research-storage-orax/{dataset}/qfforma-{model_id}.p'
 
         try:
             long_preds = pd.read_pickle(s3_file)
-            print(i / size * 100, '\n')
+            print(idx / size * 100, '\n')
             print('File found!', 'preparing to evaluate')
         except:
             continue
 
         error = evaluate_fforma_experiment(long_preds, results_dir, dataset)
         error['model_id'] = model_id
-        errors.append(error)
 
-    errors = pd.concat(errors).reset_index(drop=True)
+        error = mc.merge(error, how='left', on=['model_id'])
 
-    model_specs_df = model_specs_df.merge(errors, how='left', on=['model_id'])
-
-    model_specs_df.to_csv(f'{results_dir}/owas_experiment_qfforma.csv')
-
+        uploat_evaluation_to_s3(model_id, error, dataset)
 
 def parse_args():
     desc = "evaluate Experiment QFFORMA"
@@ -52,6 +58,8 @@ def parse_args():
 
     parser.add_argument('--dataset', type=str, help='M4, M3, etc.')
     parser.add_argument('--generate_grid', type=int, default=0, help='Generate grid')
+    parser.add_argument('--start_id', type=int, help='Start id')
+    parser.add_argument('--end_id', type=int, default=0, help='End id')
 
     return parser.parse_args()
 
@@ -71,4 +79,4 @@ if __name__ == '__main__':
 
     main(args.dataset, args.generate_grid)
 
-# PYTHONPATH=. python -m src.evaluate_experiment --dataset 'M4' --generate_grid 1
+# PYTHONPATH=. python -m src.evaluate_experiment --dataset 'M4' --start_id 1 --end_id 2 --generate_grid 1
