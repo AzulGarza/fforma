c130ca7796f371b8561b837a2473e26c1b106be1
José Morales <jmorales@grupoabraxas.com>
2020-08-17 16:15:07 -0500

    include required packages in setup to install from orax-forecast

--- setup.py
+++ setup.py
@@ -18,5 +18,8 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    install_requires=[
+        'lightgbm',
+        'tqdm',
+    ]
 )
