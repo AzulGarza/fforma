2a1d5bc1fbe1aebcdd20f0d1c045f666a09b236a
Fede <fede.garza.ramirez@gmail.com>
2020-08-23 15:50:55 -0500

    adding vis to hyperopt

--- src/hyperopt_experiment.py
+++ src/hyperopt_experiment.py
@@ -19,6 +19,12 @@ from hyperopt import (
     space_eval
 )
 
+from hyperopt.fmin import generate_trials_to_calculate
+
+import ray
+from ray import tune
+from ray.tune.schedulers import AsyncHyperBandScheduler
+from ray.tune.suggest.hyperopt import HyperOptSearch
 
 from src.benchmarks import (
     LassoQuantileRegressionAveraging,
@@ -28,20 +34,45 @@ from src.benchmarks import (
 #############################################################################
 # EXPERIMENT SPECS
 #############################################################################
-RANDOM_STATE = 42
-MAX_EVALS = 20
+RANDOM_STATE = 888
+MAX_EVALS = 100
 
 DICT_FREQS = {'H':24, 'D': 7, 'W':52, 'M': 12, 'Q': 4, 'Y': 1}
 
-GRID_QFFORMA1 = {'n_epochs' : hp.quniform('n_epochs', 5, 50, 1),
-                 'lr': hp.loguniform('lr', -5.0, -2.3),
-                 'batch_size': hp.choice('batch_size', [16, 32, 64, 128, 200, 256, 512]),
-                 'weight_decay':  hp.loguniform('weight_decay', -5.0, -2.3),
-                 'lr_scheduler_step_size': hp.quniform('lr_scheduler_step_size', 2, 20, 1),
-                 'lr_decay': hp.uniform('lr_decay', 0.01, 1.0),
-                 'dropout': hp.uniform('dropput', 0., 1),
+GRID_QFFORMA1 = {'n_epochs' : hp.quniform('n_epochs', 20, 25, 1),
+                 'lr': hp.loguniform('lr', np.log(0.02), np.log(0.05)),
+                 'momentum': hp.loguniform('momentum', np.log(0.01), np.log(1)),
+                 'batch_size': hp.choice('batch_size', [256, 512]),
+                 'nesterov': hp.choice('nesterov', [True, False]),
+                 'weight_decay':  hp.loguniform('weight_decay', np.log(0.01), np.log(0.05)),
+                 'lr_scheduler_step_size': hp.quniform('lr_scheduler_step_size', 1, 20, 1),
+                 'lr_decay': hp.uniform('lr_decay', 0.1, 0.9),
+                 'dropout': hp.uniform('dropout', 0.4, 0.5),
                  #'use_softmax': hp.choice('use_softmax', [True, False]),
-                 'train_percentile': hp.uniform('train_percentile', 0.4, 0.7)}
+                 'train_percentile': hp.uniform('train_percentile', 0.45, 0.6)}
+
+CURRENT_BEST_PARAMS = [
+    {'n_epochs': 49,
+     'lr': 0.011737006920320418,
+     'momentum': 0.1,
+     'batch_size': 1,
+     'nesterov': 0,
+     'weight_decay': 0.013538437919335407,
+     'lr_scheduler_step_size': 11,
+     'lr_decay': 0.1003490817198572,
+     'dropout': 0.4165966328090256,
+     'train_percentile': 0.57},
+    {'n_epochs': 49,
+     'lr': 0.011737006920320418,
+     'momentum': 0.1,
+     'batch_size': 1,
+     'nesterov': 0,
+     'weight_decay': 0.013538437919335407,
+     'lr_scheduler_step_size': 11,
+     'lr_decay': 0.1003490817198572,
+     'dropout': 0.4165966328090256,
+     'train_percentile': 0.49}
+]
 
 ALL_MODEL_SPECS  = {'qfforma': {'M4': GRID_QFFORMA1,
                                 'TOURISM': GRID_QFFORMA1}}
@@ -134,8 +165,9 @@ def train_qfforma(data, args):
 
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
+    ray.init()
     space = ALL_MODEL_SPECS['qfforma'][args.dataset]
-    tpe_algorithm = tpe.suggest
+
     # Parse hyper parameter data frame
     print(47*'=' + '\n')
     print('Initializing optmization process')
@@ -164,18 +196,34 @@ def train_qfforma(data, args):
                   X_test_df, preds_test_df, y_test_df[['unique_id', 'ds', 'y']],
                   verbose=False)
 
-        return {'loss': model.test_min_smape, 'params': params, 'status': STATUS_OK}
+        tune.report(loss=model.test_min_smape)
 
-    best = fmin(fn=objective, space=space, algo=tpe.suggest,
-                max_evals=MAX_EVALS,
-                rstate=np.random.RandomState(RANDOM_STATE))
+        #return {'loss': model.test_min_smape, 'params': params, 'status': STATUS_OK}
 
-    optimal_params = {**FIXED_PARAMS, **space_eval(space, best)}
+    # tpe_algorithm = tpe.suggest
+    #
+    # trials = generate_trials_to_calculate(CURRENT_BEST_PARAMS)
+    # best = fmin(fn=objective, space=space, algo=tpe.suggest, trials=trials,
+    #             max_evals=MAX_EVALS,
+    #             rstate=np.random.RandomState(RANDOM_STATE))
+    #
+    # optimal_params = {**FIXED_PARAMS, **space_eval(space, best)}
+    #
+    # pd.to_pickle(optimal_params, 'opt-params.p')
 
-    pd.to_pickle(optimal_params, 'opt-params.p')
+    #print(optimal_params)
 
-    print(optimal_params)
+    algo = HyperOptSearch(space,
+                          metric="loss",
+                          mode="min",
+                          points_to_evaluate=CURRENT_BEST_PARAMS)
+    scheduler = AsyncHyperBandScheduler(metric="loss", mode="min")
+    analysis = tune.run(objective, num_samples=MAX_EVALS, search_alg=algo, scheduler=scheduler)
 
+    df = analysis.dataframe()
+
+    df.to_csv('results.csv', index=False)
+    print(df)
 
 
     # Predict and Evaluate
