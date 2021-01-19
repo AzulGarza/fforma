dc138108a265a9075f4c8eebba4bbb3097db88e2
Fede <fede.garza.ramirez@gmail.com>
2021-01-19 12:23:03 -0600

    adding benchmark to mean

--- fforma/experiments/business/ensemble_forecasts.py
+++ fforma/experiments/business/ensemble_forecasts.py
@@ -117,7 +117,7 @@ def main(directory: str, group: str, metric: str, replace: bool) -> None:
         ######### Mean
         logger.info('Mean')
         init = time()
-        meta_learner = MetaLearnerMean()
+        meta_learner = MetaLearnerMean(benchmark)
         meta_learner = meta_learner.fit(forecasts_test)
         mean_forecasts = meta_learner.predict()
         mean_time = time() - init
